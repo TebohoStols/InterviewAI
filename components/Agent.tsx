18f9d75123cd2789e51b87d3e27eb2313de2b1fe
tebohostols <tebohogarfield@gmail.com>
2025-05-13 13:35:47 +0200

    Feedback Page

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 import { vapi } from '@/lib/vapi.sdk';
 import { join } from 'path';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -66,10 +67,11 @@ const Agent = ({userName, userId, type, interviewId, questions }: AgentProps) =>
         console.log('Generate feedback here.')
 
         //Create a server action that generates feedback
-        const {success, id } = {
-            success: true,
-            id: 'feedback-id'
-        }
+        const {success, feedbackId: id } =  await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        })
 
         if(success && id) {
             router.push(`/interview/${interviewId}/feedback`);
